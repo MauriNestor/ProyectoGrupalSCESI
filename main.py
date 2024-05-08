aa267a2948b9de7d327f23fa670d1138ee3995bc
MauriNestor <mauric845apaza@gmail.com>
2024-05-08 02:52:12 -0400

    main.py actualizado

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('esto es una pagina main')
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('menu.html')
+
+@app.route('/juego')
+def juego():
+    return render_template('juego.html')
+
+@app.route('/opciones')
+def opciones():
+    return render_template('opciones.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
