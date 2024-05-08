7ea3bb7c3f2be2f8aaabef92bf224a3522a6988e
Giulianno Morales <202107698@est.umss.edu>
2024-05-08 09:16:44 -0400

    the colocarMinas method was created

--- tablero.py
+++ tablero.py
@@ -1,11 +1,30 @@
+import random
 class tablero:
-    def crearTabla(self, filas, columnas):
-        self.filas = filas
-        self.columnas = columnas
-        self.tablero = [[' ' for _ in range(columnas)] for _ in range(filas)]
+    def crearTabla(filas, columnas,valor):
+        
+     tablero = []
+     for i in range (filas):
+         tablero.append([])
+         for j in range (columnas):
+             tablero[i].append(valor)
+             return tablero
 
-    def mostrar_tablero(self):
-        for fila in self.tablero:
-            print(" ".join(map(str, fila)))
+    def mostrarTablero(tablero):
+        for fila in tablero:
+            for elem in fila:
+             print(elem, end=" ")
+        print()
+        
+    def colocarMinas(tablero, minas,fila,columna):
+        minasOcultas = []
+        numero = 0
+        while numero < minas:
+             y = random.randint(0, fila-1)
+             x = random.randint(0, columna-1)
+             if tablero[y][x] != 9:
+                 tablero[y][x] = 9
+                 numero += 1
+                 minasOcultas.append((y,x))
+        return tablero, minasOcultas
 
 
